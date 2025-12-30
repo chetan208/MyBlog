0aad93c341840ffcbdf11f21ac8feacd60f9bb78
chetan <chetanchoudhary435@gmail.com>
2025-12-30 18:29:53 +0530

    scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 
 import Login from './pages/Login'
 import Signup from './pages/Signup'
-import { Header,Footer } from './components'
+import { Header,Footer,ScrollToTop} from './components'
 import { Outlet } from "react-router-dom";
 import { useEffect } from 'react';
 import checkAuth from './services/auth/checkAuth';
 import { useDispatch, useSelector } from 'react-redux';
-import ScrollToTop from "./components";
+
 
 
 
